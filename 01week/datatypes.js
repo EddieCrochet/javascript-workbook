b16de25174d918b90faf3c9200ff416a6db7029e
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-12-03 20:36:15 -0600

    playing in new class

---
+++ 01week/datatypes.js
@@ -0,0 +1,7 @@
+//js code to display date and time
+
+let currentDate = new Date();
+let x = new Date();
+var y = new Date();
+const pi = 3.14159;
+console.log("The current date and time is " + currentDate);
