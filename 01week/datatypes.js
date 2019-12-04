c0a49d55b320bbecda8de3bc831b181d59440ade
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-12-03 20:46:29 -0600

    first day of new JS class I am tutoring

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -5,3 +5,20 @@ let x = new Date();
 var y = new Date();
 const pi = 3.14159;
 console.log("The current date and time is " + currentDate);
+
+//js code to convert number to string
+
+var num = 15;
+var n = num.toString();
+console.log("The number is " + num);
+console.log("The string repping the number is " + n);
+
+
+
+var name = "John Doe";
+var age = 12;
+var isMale = true;
+
+console.log("The name variable is a ", typeof name);
+console.log("The name variable is a ", typeof age);
+console.log("The name variable is a ", typeof isMale);
