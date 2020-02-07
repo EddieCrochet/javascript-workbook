ed634c48be1da02f37a481ee3ddea102ecadb5f8
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-02-06 20:34:17 -0600

    documented the api fetch

--- 07week/07WeekAPI/main.js
+++ 07week/07WeekAPI/main.js
@@ -1,12 +1,16 @@
 console.log("In address");
 
 window.onload = function() {
+    //window loaded
     console.log("inside window onload");
+    //now we fetch the api
     fetchUsers();
 }
 
 function fetchUsers() {
+    //started the function that calls the api
     console.log("About to call api");
+    //fetch and handle api results --> pass response to function that gets passed in here
     fetch("https://randomuser.me/api/?results=5").then(function(response){
         console.log("Processiung response", response);
         //do something with response once I get it
@@ -14,12 +18,15 @@ function fetchUsers() {
     }).then(function(myJson){
         //do something with json payload
         console.log("Processing json payload", myJson);
+        //pass in results of my json payload to the function that will process my users
         processUsers(myJson.results);
     });
 }
 
 function processUsers(listOfUsers) {
     listOfUsers.forEach(function(user){
+        //call this function on every item in my list I pass to processusers function
         console.log("Processing a single user", user);
+        //then list individual user's info
     })
 }
